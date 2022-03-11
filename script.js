a555cf0602b8c88e1715b72caaa34efb6784274e
simutis <54304022+SimasG@users.noreply.github.com>
2022-03-11 19:41:04 +0200

    added ability to remove todos

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 // TODO: add todos to the list -> DONE
+// TODO: delete todos from the list -> DONE
+// TODO: mark todos as complete ->
+
+// TODO: clean UI stuff (limit todo size, etc.)
 
 const form = document.querySelector(".input-form");
 const todoList = document.querySelector(".todo-list");
 const input = document.querySelector(".todo-input");
-// let todo = document.querySelector("#todo-input").value;
+const deleteBtn = document.querySelectorAll(".delete");
+const todoInfoContainer = document.querySelector(".todo-info-container");
+const completeBtn = document.querySelectorAll(".circle");
+// const checkmark = document.querySelectorAll(".checkmark");
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -11,7 +18,6 @@ form.addEventListener("submit", function (e) {
 });
 
 function addNewTodo() {
-  console.log("new todo added");
   const todoHtml = `<li class="todo list-item">
     <svg xmlns="http://www.w3.org/2000/svg" width="11" height="9">
       <path
@@ -21,8 +27,11 @@ function addNewTodo() {
         d="M1 4.304L3.696 7l6-6"
       />
     </svg>
-    <p>${document.querySelector("#todo-input").value}</p>
-    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18">
+    <p>${
+      document.querySelector("#todo-input").value.charAt(0).toUpperCase() +
+      document.querySelector("#todo-input").value.slice(1)
+    }</p>
+    <svg class="delete-dynamic" xmlns="http://www.w3.org/2000/svg" width="18" height="18">
       <path
         fill="#494C6B"
         fill-rule="evenodd"
@@ -30,5 +39,32 @@ function addNewTodo() {
       />
     </svg>
   </li>`;
-  todoList.insertAdjacentHTML("afterbegin", todoHtml);
+  todoList.insertAdjacentHTML("beforeend", todoHtml);
+  document.querySelector("#todo-input").value = "";
+  const deleteBtnDynamic = document.querySelectorAll(".delete-dynamic");
+
+  deleteBtnDynamic.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      console.log("dynamic btn clicked!");
+      console.log(e.target.parentElement);
+      e.target.parentElement.remove();
+    });
+  });
 }
+
+// delete todo
+deleteBtn.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    console.log("static btn clicked");
+    console.log(e.target.parentElement);
+    e.target.parentElement.remove();
+  });
+});
+
+// mark todo as completed
+completeBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    console.log("heyoo");
+    // document.querySelector(".checkmark").classList.remove("hide");
+  });
+});
