04e0e5aedd9ff42f7e1b684214ebae67da7fc45a
simutis <54304022+SimasG@users.noreply.github.com>
2022-03-11 11:22:37 +0200

    added dynamic new todo adding

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-// TODO: add todos to the list
+// TODO: add todos to the list -> DONE
 
 const form = document.querySelector(".input-form");
 const todoList = document.querySelector(".todo-list");
+const input = document.querySelector(".todo-input");
+// let todo = document.querySelector("#todo-input").value;
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -11,22 +13,22 @@ form.addEventListener("submit", function (e) {
 function addNewTodo() {
   console.log("new todo added");
   const todoHtml = `<li class="todo list-item">
-  <svg xmlns="http://www.w3.org/2000/svg" width="11" height="9">
-    <path
-      fill="none"
-      stroke="#000"
-      stroke-width="2"
-      d="M1 4.304L3.696 7l6-6"
-    />
-  </svg>
-  <p>shit workeddddddd</p>
-  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18">
-    <path
-      fill="#494C6B"
-      fill-rule="evenodd"
-      d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"
-    />
-  </svg>
-</li>`;
+    <svg xmlns="http://www.w3.org/2000/svg" width="11" height="9">
+      <path
+        fill="none"
+        stroke="#000"
+        stroke-width="2"
+        d="M1 4.304L3.696 7l6-6"
+      />
+    </svg>
+    <p>${document.querySelector("#todo-input").value}</p>
+    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18">
+      <path
+        fill="#494C6B"
+        fill-rule="evenodd"
+        d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"
+      />
+    </svg>
+  </li>`;
   todoList.insertAdjacentHTML("afterbegin", todoHtml);
 }
